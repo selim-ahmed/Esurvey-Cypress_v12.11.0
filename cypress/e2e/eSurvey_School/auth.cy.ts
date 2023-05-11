44a719733e411219263410c8249f4499c4f1df5a
Selim <selimahmed1305@gmail.com>
2023-05-11 15:24:05 +0600

    Updates task commit

---
+++ cypress/e2e/eSurvey_School/auth.cy.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('School type user form', () => {
+    beforeEach(() => {
+        cy.visit("http://103.4.145.251:3028/")
+    })
+    
+    it("Base URL to login the Esurvey module", () => {})
+
+})
